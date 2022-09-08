3fb04b9422a18ff9189c0971702610969a59a000
mbaihaqi23 <mbaihaqi23@gmail.com>
2022-09-08 07:21:07 +0700

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,55 +15,54 @@ const AppLayouts = (props) => {
   };
 
   return (
-    // <div>
-    // <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
       
-    //   <div className="navbar-brand">
-    //     <a className="navbar-item" href="https://bulma.io">
-    //       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-    //     </a>
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+        </a>
     
-    //     <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-    //       <span aria-hidden="true"></span>
-    //       <span aria-hidden="true"></span>
-    //       <span aria-hidden="true"></span>
-    //     </a>
-    //   </div>
-    //   <div id="navbarBasicExample" className="navbar-menu">
-    //   <div className="container">
-    //     <ul className="ptb1">  
-    //     <div className="navbar-end">
-    //       <div className="navbar-item">
-    //         <div className="buttons">
-    //           <li className="navbar-item">
-    //           <Link to="/blogs" className="button is-primary">Blogs</Link>
-    //           </li>
-    //           <li style={{ display: cookies.accessToken ? "" : "none" }} className="navbar-item">
-    //           <Link to="/dashboard">Dashboard</Link>
-    //           </li>
-    //           <li style={{ display: cookies.accessToken ? "none" : "" }} className="button is-primary">
-    //           <Link to="/registration"><strong>Registration</strong></Link >
-    //           </li>
-    //           <li style={{ display: cookies.accessToken ? "none" : "" }} className="button is-light">
-    //             <Link to="/login"><strong>Login</strong></Link>
-    //           </li>
-    //           <li style={{ display: cookies.accessToken ? "" : "none" }} className="button is-danger">
-    //             <Link to="/" onClick={logout}>Logout</Link>
-    //           </li>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     </ul>
-    //  </div>
-    //   </div>
-    // </nav>  
-    // <section>{props.children}</section>
-    // <footer>
-    //     <div className="footer ptb3">Footer</div>
-    //   </footer>
-    // </div> 
+        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <li className="navbar-item">
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li style={{ display: cookies.accessToken ? "" : "none" }} className="navbar-item">
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+         </div> 
+    
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <li style={{ display: cookies.accessToken ? "none" : "" }} className="button is-primary">
+              <Link to="/registration"><strong>Registration</strong></Link >
+              </li>
+              <li style={{ display: cookies.accessToken ? "none" : "" }} className="button is-light">
+                <Link to="/login"><strong>Login</strong></Link>
+              </li>
+              <li style={{ display: cookies.accessToken ? "" : "none" }} className="button is-danger">
+                <Link to="/" onClick={logout}>Logout</Link>
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>  
+    <section>{props.children}</section>
+    <footer>
+        <div className="footer ptb3">Footer</div>
+      </footer>
+    </div> 
 
-     <div className="mainWrapper">
+    /* <div className="mainWrapper">
       <header>
         <div className="appTitle">My React App</div>
       </header>
@@ -95,12 +94,8 @@ const AppLayouts = (props) => {
       <footer>
         <div className="footer ptb3">Footer</div>
       </footer>
-    </div>
+    </div> */
   );
 };
 
 export default AppLayouts;
-
-
-
-
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -48,30 +48,39 @@ const Dashboard = () => {
           Post not found!
         </div>
 
-        <div className="container">
-            <h1 className="text-center mt-3">my blog</h1>
-            <div className="row mt-5">
-                {posts.map((post) => {
-                    return (
-                        <div className="col" key={post.id}>
-                        <Card style={{ width: '30rem', height: '20rem' }} className="m-2">
-                        <Card.Img style={{backgroundImage: `url(${post.image})`, height: '15rem'}} variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                            <Card.Title style={{ height: '1rem', display: 'flex', alignItems: 'center'}}><a href={`/blogs/${post.id}`}>{post.title}</a></Card.Title>
-                            <Card.Text>
-                                {post.body}
-                            </Card.Text>
-                            <div className="p3">
-                              <Link to={`/posts/${post.id}/edit`}>Edit</Link>
-                            </div>
-                            </Card.Body>
-                        </Card>
-                        </div>
-                    )
-                })}
-            </div>
+        <div className="boxWrapper">
+          {posts
+            .sort((a, b) => b.id - a.id)
+            .map((post) => {
+              return (
+                <div key={post.id} className="boxStyle">
+                  <div className="imageArticle">
+                    <div
+                      className="image"
+                      style={{
+                        backgroundImage: `url(${post.image})`,
+                      }}
+                    ></div>
+                  </div>
+                  <div className="postMeta">
+                    <Link to="/" className="category">
+                      {(post.category = "Category")}
+                    </Link>
+                  </div>
+                  <h3 className="title">
+                    <a href={`/blogs/${post.id}`}>{post.title}</a>
+                  </h3>
+                  <div className="body-posts p3">
+                    <p>{post.body}</p>
+                  </div>
+                  <div className="p3">
+                    <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+                  </div>
+                </div>
+              );
+            })}
         </div>
-        </div>
+      </div>
     </>
   );
 };
